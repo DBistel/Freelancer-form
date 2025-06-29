10ed5c0380cb9b5b2684fae8f492a4682d130079
dbistel <ddbistel@gmail.com>
2025-06-29 15:44:42 -0600

    created a function that returns an average rate. added a variable that holds the function and passes though the array from earlier.

--- index.js
+++ index.js
@@ -11,15 +11,22 @@ const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
 
-function makeFreeLancer(){
+const makeFreeLancer = () => {
     const name = NAMES[Math.floor(Math.random() * NAMES.length)];
     const occupation = OCCUPATIONS[Math.floor(Math.random() * NAMES.length)];
     const rate = Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min + 1)) + PRICE_RANGE.min;
     return {name, occupation, rate};
 }
 
-// const freeLancers = Array.from({ length:NUM_FREELANCERS},makeFreeLancer);
+// const generateFreeLancers = Array.from({ length:NUM_FREELANCERS},makeFreeLancer);
 const generateFreeLancers = [];
-for (let i=0; i< NUM_FREELANCERS; i++){
+for (let i=0; i< NUM_FREELANCERS; i++){ // i use i < num insted of i <= num because i<num makes 0-99 index of the array = 100 where i<=num will result in an index of 100 = 101
     generateFreeLancers.push(makeFreeLancer())
 }
+
+const averageRate = (arr) => {
+    const total = arr.reduce((sum, freelancer) => sum + freelancer.rate,0);
+    return total / generateFreeLancers.length
+}
+const avg = averageRate (generateFreeLancers)
+
