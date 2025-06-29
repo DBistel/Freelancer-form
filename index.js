f8aca666367458dd65f7965c211ceab7e501e490
dbistel <ddbistel@gmail.com>
2025-06-29 13:41:15 -0600

    created a variable to hold an array of makefreelancer based on the number of freelancers.

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+function makeFreeLancer(){
+    const name = NAMES[Math.floor(Math.random() * NAMES.length)];
+    const occupation = OCCUPATIONS[Math.floor(Math.random() * NAMES.length)];
+    const rate = Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min + 1)) + PRICE_RANGE.min;
+    return {name, occupation, rate};
+}
+
+// const freeLancers = Array.from({ length:NUM_FREELANCERS},makeFreeLancer);
+const generateFreeLancers = [];
+for (let i=0; i< NUM_FREELANCERS; i++){
+    generateFreeLancers.push(makeFreeLancer())
+}
